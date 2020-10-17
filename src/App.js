07f077e53bad2245f5378b0648f4e648b6d53ebd
huntsimon23 <hunt.simon23@gmail.com>
2020-10-16 23:44:42 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <h2 style={{"marginTop":"50px"}}>
+          SimonHunt.us 
+        </h2>
+        <h5>Schedule a 30 minute virtual or in person meeting</h5>
+        <div className="calendly-inline-widget" data-url="https://calendly.com/firstorlandocr/simonhunt?hide_event_type_details=1" style={{"minWidth":"320px","height":"630px"}}></div>
       </header>
     </div>
   );
